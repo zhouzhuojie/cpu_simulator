d8ac3c8464fb336414d1546dd155790c5abef0c8
zhouzhuojie <zhouzhuojie@gmail.com>
2011-04-25 23:18:58 -0400

    Modified VADD and VSUB using EA mechanism.

--- src/arc_project/Test.java
+++ src/arc_project/Test.java
@@ -56,15 +56,19 @@ public class Test {
 		
 //		Global.GUIMAIN.refresh();
 		
-		char[] w0 = {'0','0','1','0','0','0','0','0','0','0','0','0','0','0','0','0'};
-		char[] w1 = {'1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0'};
+		char[] w0 = {'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'};
+		char[] w1 = {'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'};
+		char[] w2 = {'0','0','0','0','0','0','0','0','0','0','0','0','0','0','1','0'};
+		char[] w3 = {'0','0','0','0','0','0','0','0','0','0','0','0','0','0','1','1'};
 		Global.MEMORY.set(w0,w0);
+		Global.MEMORY.set(w2,w2);
 		Global.MEMORY.set(w1,w1);
-		Global.R[0].set(w0);
-		Global.R[1].set(w1);
+		Global.MEMORY.set(w3,w3);
+//		Global.R[0].set(w0);
+//		Global.R[1].set(w1);
 		
 //		Global.IR.set(c);
-		String s = "0000000001000000";
+		String s = "0000000000000000";
 		Global.IR.set(s.toCharArray());
 		InstructionSet_VSUB vadd= new InstructionSet_VSUB();vadd.operate();
 //		InstructionSet_VADD vadd= new InstructionSet_VADD();vadd.operate();
--- src/isa/InstructionSet_VSUB.java
+++ src/isa/InstructionSet_VSUB.java
@@ -22,24 +22,33 @@ public class InstructionSet_VSUB extends Instruction {
 	 */
 	public void operate() throws Exception {
 
-		Global.RSR.set(Global.IR.get(6, 7));
-		Global.RSR2.set(Global.IR.get(8, 9));
-
+		//IND <- IR 6
+		Global.IND.set(Global.IR.get(6),0);
+		//IXI <- IR 7
+		Global.IXI.set(Global.IR.get(7),0);		
+		//ADDR <- IR 10-15
+		Global.ADDR.set(Global.IR.get(10,15));
 		for (int i = 0; i < 32; i++) {
 			Global.VREG1[i] = new Register(16);
 			Global.VREG2[i] = new Register(16);
-			
-			Global.MAR.set(Global.ALU.int2char(i
-					+ Global.ALU.char2int(Global.R[Global.ALU
-							.char2int(Global.RSR.get())].get())));
+		    
+
+            Global.MAR.set(Instruction.EA());
+			Global.MBR.set(Global.L1.get(Global.MAR.get()));
+			Global.MAR.set(Global.ALU.int2char(
+                        i + Global.ALU.char2int(Global.MBR.get())));
 			Global.MBR.set(Global.L1.get(Global.MAR.get()));
 			Global.VREG1[i].set(Global.MBR.get());
 
-			Global.MAR.set(Global.ALU.int2char(i
-					+ Global.ALU.char2int(Global.R[Global.ALU
-							.char2int(Global.RSR2.get())].get())));
+			Global.MAR.set(Global.ALU.int2char(
+                        1 + Global.ALU.char2int(Instruction.EA())));
+			Global.MBR.set(Global.L1.get(Global.MAR.get()));
+			Global.MAR.set(Global.ALU.int2char(
+                        i + Global.ALU.char2int(Global.MBR.get())));
 			Global.MBR.set(Global.L1.get(Global.MAR.get()));
 			Global.VREG2[i].set(Global.MBR.get());
+
+
 
 			Global.VREG1[i].set(Global.ALU.num2char(Global.ALU
 					.char2num(Global.VREG1[i].get())
